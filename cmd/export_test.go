3d685b9584ef43b6b871c44943b0cd67daab8f6e
Christopher Lamm <christopherlamm81@icloud.com>
2025-09-28 12:12:51 -0400

    remove started test

--- cmd/export_test.go
+++ cmd/export_test.go
@@ -1,15 +1,1 @@
 package cmd
-
-import (
-	"testing"
-
-	"github.com/spf13/afero"
-)
-
-func TestRunExport(t *testing.T) {
-	t.Run("exports Granola documents", func(t *testing.T) {
-		appFS = afero.NewMemMapFs()
-
-		afero.WriteFile(appFS, "/test/spabase.json", []byte(`{"workos_tokens": {}}`), 0644)
-	})
-}
